57c3bccdbc9b244984b100668151438464f128d9
Patrick T <patrick.trautmann@gmail.com>
2012-04-13 19:35:41 +0200

    Blank OpenGL Window

--- Engine/Engine.cs
+++ Engine/Engine.cs
@@ -1,11 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
 
 namespace DynaStudios
 {
-    public class Engine
+    public class Engine : GameWindow
     {
+
+        public Engine() : base(800, 600, new GraphicsMode(32, 0, 0, 4))
+        {
+            //todo: Set Keyboard Input Bindings here
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color.Gray);
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Begin(BeginMode.Triangles);
+
+            GL.Color3(Color.MidnightBlue);
+            GL.Vertex2(-1.0f, 1.0f);
+            GL.Color3(Color.SpringGreen);
+            GL.Vertex2(0.0f, -1.0f);
+            GL.Color3(Color.Ivory);
+            GL.Vertex2(1.0f, 1.0f);
+
+            GL.End();
+
+            this.SwapBuffers();
+        }
+
     }
 }
---
+++ TestGame/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DynaStudios;
+
+namespace TestGame
+{
+    public class Game : Engine
+    {
+
+        public Engine Engine;
+
+        public Game()
+        {
+            this.Engine = new Engine();
+        }
+
+        static void Main(string[] args)
+        {
+
+            Game game = new Game();
+            game.Engine.Run();
+
+
+        }
+    }
+}
