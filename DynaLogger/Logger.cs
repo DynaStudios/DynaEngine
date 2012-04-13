3f03e71a5fc3d62835e1616d3a1eb432e74a4043
Patrick T <patrick.trautmann@gmail.com>
2012-04-13 18:18:54 +0200

    Renamed Files

--- DynaLogger/Logger.cs
+++ DynaLogger/Logger.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Engine
+namespace DynaStudios.DynaLogger
 {
-    public class Class1
+    public class Logger
     {
     }
 }
