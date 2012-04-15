d3c9f6012063eb04e14f59c38e4342e7dc1a6508
Patrick T <patrick.trautmann@gmail.com>
2012-04-15 02:51:54 +0200

    Fixed previous commit

---
+++ Engine/UI/Controls/IUIControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Input;
+
+namespace DynaStudios.UI.Controls
+{
+    public interface IUIControl
+    {
+        /// <summary>
+        /// Renders the UIControl
+        /// </summary>
+        void render();
+
+        /// <summary>
+        /// Called when the UIControl got clicked
+        /// </summary>
+        /// <param name="arg">Mouse Informations</param>
+        void onClicked(MouseButtonEventArgs arg);
+
+        /// <summary>
+        /// Called when the UIControl received Keyboard Input
+        /// </summary>
+        /// <param name="arg"></param>
+        void onKeyPressed(KeyboardKeyEventArgs arg);
+    }
+}
