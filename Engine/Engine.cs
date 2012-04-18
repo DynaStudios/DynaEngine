ad77e6f42ddd922cf8a431d9e93d88d878aeaf05
Patrick T <patrick.trautmann@gmail.com>
2012-04-18 20:40:17 +0200

    Fixed DepthTest Blocks

--- Engine/Engine.cs
+++ Engine/Engine.cs
@@ -88,26 +88,26 @@ namespace DynaStudios
         private Chunklet chunklet1;
 
         public Engine()
-            : base(1024, 768, new GraphicsMode(32, 0, 0, 4))
+            : base(1024, 768)
         {
             Logger = new Logger();
             Logger.Register(new ConsoleLogger());
             Logger.Register(new FileSystemLogger());
             Logger.Debug("Init Game.");
-//             Block block1 = new Block(0, 0, 0);
-//             Block block2 = new Block(0, 0, 2);
-//             Block block3 = new Block(2, 0, 0);
-//             Block block4 = new Block(0, 2, 0);
-//             block1.color = Color.AliceBlue;
-//             block2.color = Color.White;
-//             block3.color = Color.Red;
-//             block4.color = Color.Brown;
-//             worldObjects.Add(block3);
-//             worldObjects.Add(block1);
-//             worldObjects.Add(block4);
-//             worldObjects.Add(block2);
+            Block block1 = new Block(0, 0, 0);
+            Block block2 = new Block(0, 0, 2);
+            Block block3 = new Block(2, 0, 0);
+            Block block4 = new Block(0, 2, 0);
+            block1.color = Color.AliceBlue;
+            block2.color = Color.White;
+            block3.color = Color.Red;
+            block4.color = Color.Brown;
+            worldObjects.Add(block3);
+            worldObjects.Add(block1);
+            worldObjects.Add(block4);
+            worldObjects.Add(block2);
 
-            chunklet1 = new Chunklet();
+            //chunklet1 = new Chunklet();
 
 
         }
@@ -116,7 +116,6 @@ namespace DynaStudios
         {
             Logger.Debug("Called OnLoad();");
             base.Title = "DynaEngine Sample Game";
-            GL.ClearColor(Color.Gray);
 
             //Init User Interface Controller
             Logger.Debug("Register GUI Controller");
@@ -128,6 +127,14 @@ namespace DynaStudios
             Camera.WorldObject = camerMan;
 
             resize(null, EventArgs.Empty);
+
+            GL.Enable(EnableCap.DepthTest);
+            //GL.ClearDepth(1.0);
+            //GL.DepthFunc(DepthFunction.Lequal);
+            //GL.Disable(EnableCap.DepthTest);
+            GL.ClearColor(Color.Gray);
+
+
         }
 
         protected override void OnResize(EventArgs e)
@@ -146,15 +153,17 @@ namespace DynaStudios
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             //Matrix4 lookat = Matrix4.LookAt(0, 5, 5, 0, 0, 0, 0, 1, 0);
-            GL.MatrixMode(MatrixMode.Modelview);
+            //GL.MatrixMode(MatrixMode.Modelview);
             //GL.LoadMatrix(ref lookat);
-
-            GL.Enable(EnableCap.DepthTest);
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
             //moves the camera
             camera.move();
+
+            bool Result = GL.IsEnabled(EnableCap.DepthTest);
+            //Logger.Debug("DepthTest is " + Result);
+            //GL.DepthRange(1.0, 0.0);
 
             //Render World Objects
             foreach (AbstractDrawable worldObject in worldObjects)
@@ -163,7 +172,7 @@ namespace DynaStudios
                 worldObject.doRender();
             }
 
-            chunklet1.render(camerMan);
+            //chunklet1.render(camerMan);
 
             // unmoves the camera for the next frame
             camera.moveBack();
@@ -187,6 +196,7 @@ namespace DynaStudios
             Matrix4 perpective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspect_ratio, 1, 64);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadMatrix(ref perpective);
+            //GL.LoadIdentity();
         }
     }
 }
