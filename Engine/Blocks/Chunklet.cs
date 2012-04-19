2948ecf8e7b6e517041d2d054c20c41ec9a5b39c
Arthur Landel <arthur@dyna-studios.com>
2012-04-19 20:54:44 +0200

    methode to generate a random chunklet

--- Engine/Blocks/Chunklet.cs
+++ Engine/Blocks/Chunklet.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+using System.Drawing;
+
 namespace DynaStudios.Blocks
 {
     public class Chunklet
@@ -18,6 +20,30 @@ namespace DynaStudios.Blocks
             _blocks = new Block[16, 16, 16];
         }
 
+        /// <summary>
+        /// for testing purpose only
+        /// include it in the constructor to generate a randomly filled chunk
+        /// </summary>
+        private void generateStupedWorld()
+        {
+            Random rnd = new Random();
+            Color[] colors = new Color[] {Color.Black, Color.White, Color.Red, Color.Green, Color.Blue, Color.Yellow};
+            for (int x = 0; x < 16; ++x)
+            {
+                for (int y = 0; y < 16; ++y)
+                {
+                    for (int z = 0; z < 16; ++z)
+                    {
+                        if (rnd.Next(3) == 0)
+                        {
+                            Block block = new Block(x, y, z);
+                            block.color = colors[rnd.Next(colors.Length)];
+                            _blocks[x, y, z] = block;
+                        }
+                    }
+                }
+            }
+        }
 
         internal void render(CameraMan camerMan)
         {
