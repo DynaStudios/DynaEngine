1365da71e97cd4c30300b21e33a7156888d08184
Patrick T <patrick.trautmann@gmail.com>
2012-05-06 03:26:59 +0200

    Add missing Files

---
+++ TestGame/Scenes/PhysicTestScene.cs
@@ -0,0 +1,136 @@
+﻿using BulletSharp;
+using DynaStudios;
+
+namespace TestGame.Scenes
+{
+    public class PhysicTestScene : IScene
+    {
+        public Engine Engine { get; set; }
+
+        //Physics Stuff
+
+        #region Physics
+
+        // create 125 (5x5x5) dynamic objects
+        int ArraySizeX = 5, ArraySizeY = 5, ArraySizeZ = 5;
+
+        // scaling of the objects (0.1 = 20 centimeter boxes )
+        float StartPosX = -5;
+
+        float StartPosY = -5;
+        float StartPosZ = -3;
+
+        protected CollisionConfiguration CollisionConf;
+
+        protected CollisionDispatcher Dispatcher;
+        protected BroadphaseInterface Broadphase;
+        protected ConstraintSolver Solver;
+
+        public AlignedCollisionShapeArray CollisionShapes { get; private set; }
+
+        DynamicsWorld _world;
+
+        #endregion Physics
+
+        public DynamicsWorld World
+        {
+            get { return _world; }
+            protected set { _world = value; }
+        }
+
+        public PhysicTestScene(Engine engine)
+        {
+            Engine = engine;
+            Engine.Logger.Debug("Loaded PhysicsTestScene");
+        }
+
+        private void InitTestScene()
+        {
+            CollisionConf = new DefaultCollisionConfiguration();
+            Dispatcher = new CollisionDispatcher(CollisionConf);
+
+            Broadphase = new DbvtBroadphase();
+
+            World = new DiscreteDynamicsWorld(Dispatcher, Broadphase, null, CollisionConf) { Gravity = new Vector3(0, -10, 0) };
+            //Create the ground
+            CollisionShape groundShape = new BoxShape(50, 1, 50);
+            CollisionShapes.Add(groundShape);
+            CollisionObject ground = LocalCreateRigidBody(0, Matrix.Identity, groundShape);
+            ground.UserObject = "Ground";
+
+            // create a few dynamic rigidbodies
+            float mass = 1.0f;
+
+            CollisionShape colShape = new BoxShape(1);
+            CollisionShapes.Add(colShape);
+            Vector3 localInertia = colShape.CalculateLocalInertia(mass);
+
+            float start_x = StartPosX - ArraySizeX / 2;
+            float start_y = StartPosY;
+            float start_z = StartPosZ - ArraySizeZ / 2;
+
+            int k, i, j;
+            for (k = 0; k < ArraySizeY; k++)
+            {
+                for (i = 0; i < ArraySizeX; i++)
+                {
+                    for (j = 0; j < ArraySizeZ; j++)
+                    {
+                        Matrix startTransform = Matrix.Translation(
+                            2 * i + start_x,
+                            2 * k + start_y,
+                            2 * j + start_z
+                        );
+
+                        // using motionstate is recommended, it provides interpolation capabilities
+                        // and only synchronizes 'active' objects
+                        DefaultMotionState myMotionState = new DefaultMotionState(startTransform);
+                        RigidBodyConstructionInfo rbInfo =
+                            new RigidBodyConstructionInfo(mass, myMotionState, colShape, localInertia);
+                        RigidBody body = new RigidBody(rbInfo);
+
+                        // make it drop from a height
+                        body.Translate(new Vector3(0, 20, 0));
+
+                        World.AddRigidBody(body);
+                    }
+                }
+            }
+        }
+
+        private RigidBody LocalCreateRigidBody(float mass, Matrix startTransform, CollisionShape shape)
+        {
+            //rigidbody is dynamic if and only if mass is non zero, otherwise static
+            bool isDynamic = (mass != 0.0f);
+
+            Vector3 localInertia = Vector3.Zero;
+            if (isDynamic)
+                shape.CalculateLocalInertia(mass, out localInertia);
+
+            //using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+            DefaultMotionState myMotionState = new DefaultMotionState(startTransform);
+
+            RigidBodyConstructionInfo rbInfo = new RigidBodyConstructionInfo(mass, myMotionState, shape, localInertia);
+            RigidBody body = new RigidBody(rbInfo);
+
+            _world.AddRigidBody(body);
+
+            return body;
+        }
+
+        public void loadScene()
+        {
+            Engine.Logger.Debug("Start loading Level Content");
+            CollisionShapes = new AlignedCollisionShapeArray();
+            InitTestScene();
+        }
+
+        public void doRender()
+        {
+        }
+
+        public void unloadScene()
+        {
+        }
+    }
+}
